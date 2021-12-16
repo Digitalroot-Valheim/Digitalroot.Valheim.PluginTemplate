4d0a4c7157b9e0f3fe39af7c735af4e99e538877
Nicholas Dunnaway <nick@dunnaway.org>
2021-12-15 23:51:15 -0800

    Initial Release

---
+++ src/Digitalroot.Valheim.PluginTemplate/Main.cs
@@ -0,0 +1,131 @@
+﻿using BepInEx;
+using Digitalroot.Valheim.Common;
+using HarmonyLib;
+using JetBrains.Annotations;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace Digitalroot.Valheim.PluginTemplate
+{
+  [BepInPlugin(Guid, Name, Version)]
+  public class Main : BaseUnityPlugin, ITraceableLogging
+  {
+    public const string Version = "1.0.0";
+    public const string Name = "Digitalroot Plug-in Template";
+    public const string Guid = "digitalroot.mods.PluginTemplate";
+    public const string Namespace = "Digitalroot.Valheim" + nameof(PluginTemplate);
+    private Harmony _harmony;
+    public static Main Instance;
+
+    public Main()
+    {
+      Instance = this;
+      #if DEBUG
+      EnableTrace = true;
+      Log.RegisterSource(Instance);
+      #else
+      EnableTrace = false;
+      #endif
+      Log.Trace(Main.Instance, $"{Main.Namespace}.{MethodBase.GetCurrentMethod().DeclaringType?.Name}.{MethodBase.GetCurrentMethod().Name}");
+    }
+
+    [UsedImplicitly]
+    private void Awake()
+    {
+      try
+      {
+        Log.Trace(Main.Instance, $"{Main.Namespace}.{MethodBase.GetCurrentMethod().DeclaringType?.Name}.{MethodBase.GetCurrentMethod().Name}");
+        _harmony = Harmony.CreateAndPatchAll(typeof(Main).Assembly, Guid);
+      }
+      catch (Exception e)
+      {
+        Log.Error(Instance, e);
+      }
+    }
+
+    [UsedImplicitly]
+    private void OnDestroy()
+    {
+      try
+      {
+        Log.Trace(Main.Instance, $"{Main.Namespace}.{MethodBase.GetCurrentMethod().DeclaringType?.Name}.{MethodBase.GetCurrentMethod().Name}");
+        _harmony?.UnpatchSelf();
+      }
+      catch (Exception e)
+      {
+        Log.Error(Instance, e);
+      }
+    }
+
+    public void OnSpawnedPlayer(ref Game game, Vector3 spawnPoint)
+    {
+      try
+      {
+        throw new NotImplementedException();
+      }
+      catch (Exception e)
+      {
+        Log.Error(Instance, e);
+      }
+    }
+
+    public void OnZNetAwake(ref ZNet zNet)
+    {
+      try
+      {
+        throw new NotImplementedException();
+      }
+      catch (Exception e)
+      {
+        Log.Error(Instance, e);
+      }
+    }
+
+    public void OnZNetSceneAwake(ref ZNetScene zNetScene)
+    {
+      try
+      {
+        throw new NotImplementedException();
+      }
+      catch (Exception e)
+      {
+        Log.Error(Instance, e);
+      }
+    }
+
+    public void OnObjectDBCopyOtherDB(ref ObjectDB objectDB)
+    {
+      try
+      {
+        throw new NotImplementedException();
+      }
+      catch (Exception e)
+      {
+        Log.Error(Instance, e);
+      }
+    }
+
+    public void OnObjectDBAwake(ref ObjectDB objectDB)
+    {
+      try
+      {
+        throw new NotImplementedException();
+      }
+      catch (Exception e)
+      {
+        Log.Error(Instance, e);
+      }
+    }
+
+    #region Implementation of ITraceableLogging
+
+    /// <inheritdoc />
+    public string Source => Namespace;
+
+    /// <inheritdoc />
+    public bool EnableTrace { get; }
+
+    #endregion
+  }
+}
